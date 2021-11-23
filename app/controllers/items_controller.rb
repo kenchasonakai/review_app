d68b0e73e612746c6f7e5f4ed035ac2c48656da5
kenchaso <kenchasonakai@gmail.com>
2021-11-23 09:48:43 +0900

    item検索とページネーション用のロジックを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
   def index
+    page = params[:current_page]&.to_i || 1
+    page_type = params[:page_type]
+    keyword = params[:keyword] || "楽天"
+    @items = RakutenWebService::Ichiba::Item.search(keyword: keyword, hits: 20, page: curricurate_page(page,page_type))
+  end
+
+  private
+
+  def curricurate_page(page, page_type)
+    if page > 1 && page_type == "prev"
+      page -= 1
+    elsif page_type == "next"
+      page += 1
+    else
+      page
+    end
   end
 end
