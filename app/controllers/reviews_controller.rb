a477514385fbca88f3bc05d45bffd19a70c42593
kenchaso <kenchasonakai@gmail.com>
2021-11-24 15:46:54 +0900

    review用のコントローラーを作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,20 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to item_path(@review.item_code)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:user_name, :content, :item_code, :score)
+  end
 end
