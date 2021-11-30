729aed015337414880ed6b5e1f34185312e98fd4
kenchaso <kenchasonakai@gmail.com>
2021-11-30 17:03:15 +0900

    add: itemの一覧ページにreviewのインスタンスを渡すようにした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,14 @@ class ItemsController < ApplicationController
   def index
     page = params[:current_page]&.to_i || 1
     page_type = params[:page_type]
-    keyword = params[:keyword] || "楽天"
+    keyword = params[:keyword].present? ? params[:keyword] : "楽天"
     @items = RakutenWebService::Ichiba::Item.search(keyword: keyword, hits: 20, page: curricurate_page(page,page_type))
+    @reviews = Review.all
+  end
+
+  def show
+    @item = RakutenWebService::Ichiba::Item.search(item_code: params[:item_code])
+    @item_reviews = Review.where("item_code = ?", params[:item_code])
   end
 
   private
